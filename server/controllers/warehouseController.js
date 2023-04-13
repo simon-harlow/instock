faadab22f0143785b88cb7dd20067042483cc2d2
Evan Cheng <evancheng2014@gmail.com>
2023-04-12 22:43:09 -0700

    addWarehouse controller and routes

---
+++ server/controllers/warehouseController.js
@@ -0,0 +1,21 @@
+const knex = require("knex")(require("../knexfile"));
+
+exports.addWarehouse = (req, res) => {
+  if (
+    !req.body.warehouse_name ||
+    !req.body.address ||
+    !req.body.city ||
+    !req.body.country ||
+    !req.body.contact_name ||
+    !req.body.contact_position ||
+    !req.body.contact_email ||
+    !req.body.contact_phone
+  ) {
+    return res.status(400).send("Please enter non-empty fields");
+  }
+
+  knex("warehouse").then((data) => {
+    const newWarehouseURL = `/warehouses/${data[0]}`;
+    res.status(201).location(newWarehouseURL).send(newWarehouseURL);
+  });
+};
---
+++ server/routes/warehouseRoute.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const warehouseController = require("../controllers/warehouseController");
+
+router.route("/").post(warehouseController.addWarehouse);
