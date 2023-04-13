04fd630376309410f44c59118bac15583265e212
Evan Cheng <evancheng2014@gmail.com>
2023-04-12 21:23:47 -0700

    use dotenv for PORT

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env || 8080;
 
 app.use(cors());
 
