eaa40285947fb3cdad575d2e00287c6f4fc5fe82
Simon Harlow <simonharlow11@gmail.com>
2023-04-14 11:23:06 -0700

    needed to get my app.js file to same level as develop to avoid merge conflict in GitHub

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Arrow_back, Arrow_drop, Chevron_right, Close } from "./assets/modifiedLogos"
+import { Button } from '@chakra-ui/react';
+
 
 function App() {
-  return <div>
-    Hello World!
-  </div>;
-}
+  return (
+    <>
+  <div >InStock 4 initial project setup</div>
+  <Button bg="$InstockIndigo" fontFamily="Titillium Web" fontWeight="normal" leftIcon={<Arrow_back/>}>Button</Button>
+  <Button bg="$InstockIndigo" fontFamily="Titillium Web" fontWeight="normal" leftIcon={<Arrow_drop/>}>Button</Button>
+  <Button bg="$InstockIndigo" textStyle='Titillium Web' fontWeight='semibold' leftIcon={<Chevron_right/>}>Button</Button>
+  <Button bg="$InstockIndigo" textStyle='Titillium Web' fontWeight='semibold' leftIcon={<Close/>}>Button</Button>
+
+  </>
+  )
+};
 
 export default App;
