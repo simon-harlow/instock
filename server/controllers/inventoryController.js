d0b92cd57187012ccec2dad2cc55d830de301e8f
Simon Harlow <simonharlow11@gmail.com>
2023-04-15 12:24:35 -0700

    added handler for inventory edit that meets requirements in JIRA ticket #29, added re-usable uuid validater to validatiosn file, updated and re-formatted inventoryRoutes

--- server/controllers/inventoryController.js
+++ server/controllers/inventoryController.js
@@ -1,4 +1,5 @@
 const { v4: uuid } = require('uuid');
+const validation = require('./validation');
 const knex = require('knex')(require('../knexfile'));
 
 const selectInventory = () =>
@@ -42,4 +43,40 @@ const singleInventory = (req, res) => {
         );
 };
 
-module.exports = { index, singleInventory };
+const updateInventory = (req, res) => {
+    if (
+        !validation.uuidValidate(req.body.warehouse_id) ||
+        !validation.nonEmptyValidate(req.body.item_name) ||
+        !validation.nonEmptyValidate(req.body.description) ||
+        !validation.nonEmptyValidate(req.body.category) ||
+        !validation.nonEmptyValidate(req.body.status) ||
+        !validation.quantityValidate(req.body.quantity)
+    ) {
+        return res.status(400).send('Please enter valid fields');
+    }
+
+    // Check if warehouse_id is a valid foreign key value in the warehouses table
+    knex('warehouses')
+        .where({ id: req.body.warehouse_id })
+        .select('id')
+        .then((rows) => {
+            if (!rows.length) {
+                return res.status(400).send(`warehouse_id: ${req.body.warehouse_id} does not exist in the warehouses table`);
+            }
+
+            return knex('inventories')
+                .update(req.body)
+                .where({ id: req.params.id })
+                .then(() => {
+                    res.status(200).send(`Inventory with id: ${req.params.id} has been updated`);
+                })
+                .catch((err) =>
+                    res.status(400).send(`Error updating Inventory ${req.params.id} ${err}`)
+                );
+        })
+        .catch((err) =>
+            res.status(400).send(`Error retrieving Inventory ${req.params.id} ${err}`)
+        );
+};
+
+module.exports = { index, singleInventory, updateInventory };
