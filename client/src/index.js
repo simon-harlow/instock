48a07795a7163aa9f9e45527a2b11988a0a0d70c
Irene Huynh <imhuynh@outlook.com>
2023-04-15 12:47:47 -0700

    fixed some bugs in font

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ChakraProvider} from "@chakra-ui/react";
 import App from "./App";
-import '../src/styles/titilliumWebFont.css';
 import { themes } from "./styles/themes"; 
-
+import Fonts from "./styles/fonts";
 ReactDOM.render(
   
   <React.StrictMode>
     <ChakraProvider theme={themes}>
+      <Fonts/>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
