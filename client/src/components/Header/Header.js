0b5109b55b532c56729d8fd17c553f65a53d8284
Irene Huynh <imhuynh@outlook.com>
2023-04-25 20:19:19 -0700

    added leveling changes and also margin changes to desktop page

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,67 +1,69 @@
 import React from 'react';
-import { Box, Image, Stack, Button, Link} from '@chakra-ui/react';
+import { Box, Image, Stack, Button, Link, Flex} from '@chakra-ui/react';
 import { NavLink } from 'react-router-dom';
 import Logo2 from '../../assets/Logo/InStock-Logo_2x.png';
 
 function Header() {
     return (
-        <Box
-            bg ="$Graphite"
-            width ="100%"
-            height={{sm: "200px", md: "186px"}}
-            pb={{md: "90px"}}
-            align="center"
-            px={{sm: "4", md: "8", xl: "130px" }}
-            display={{sm: null, md: "flex"}}
-            justifyContent={{sm: "center", md: "space-between"}}
-        >
-            <Image 
-            py={{sm: "6%", md:"revert"}}
-            width={{sm: "128px", md:"128px", lg: "128px"}}
-            objectFit="contain"
-            src={Logo2} 
-            />
-            <Stack
-                spacing ={2}
-                direction = "row"
-                align= {{sm: "center", md: null}}
-                justifyContent={{ sm: "space-evenly",  md: "space-evenly", lg: "space-evenly"}}
-            >
-            <Link
-                to="/warehouses/"
-                as={NavLink}
-                color="white"
-                size="md"
-                py="2"
-                width={{sm: "164px", md:"107px"}}
-                fontSize={{sm:"h3Labels"}}
-                lineHeight={{sm:"h3Labels"}}
-                borderRadius="20"
-                _activeLink={{
-                        bgColor:"$InstockBlack"
+        <Flex bg ="$Graphite" justifyContent="center">
+            <Box
+                width={{sm:"100%",xl:"1020px"}}
+                height={{sm: "200px", md: "186px"}}
+                pb={{md: "90px"}}
+                align="center"
+                mx={{sm: "16px", md: "8", xl:"revert"}}
+                display={{sm: null, md: "flex"}}
+                justifyContent={{sm: "center", md: "space-between"}}
 
-                        }}
             >
-                Warehouses
-            </Link>
-            <Link
-                to="/inventories/"
-                as={NavLink}
-                color="white"
-                size="md"
-                py="2"
-                width={{sm: "164px", md:"107px"}}
-                fontSize={{sm:"h3Labels"}}
-                lineHeight={{sm:"h3Labels"}}
-                borderRadius="20"
-                _activeLink={{
-                        bgColor:"$InstockBlack"
+                <Image 
+                py={{sm: "6", md:"revert"}}
+                width={{sm: "128px", md:"128px", lg: "128px"}}
+                objectFit="contain"
+                src={Logo2} 
+                />
+                <Stack
+                    spacing ={2}
+                    direction = "row"
+                    align= {{sm: "center", md: null}}
+                    justifyContent={{ sm: "space-evenly",  md: "space-evenly", lg: "space-evenly"}}
+                >
+                <Link
+                    to="/warehouses/"
+                    as={NavLink}
+                    color="white"
+                    size="md"
+                    py="2"
+                    width={{sm: "165px", md:"107px"}}
+                    fontSize={{sm:"h3Labels"}}
+                    lineHeight={{sm:"h3Labels"}}
+                    borderRadius="20"
+                    _activeLink={{
+                            bgColor:"$InstockBlack"
 
-                        }}>
-                Inventory
-            </Link>
-            </Stack>
-        </Box>
+                            }}
+                >
+                    Warehouses
+                </Link>
+                <Link
+                    to="/inventories/"
+                    as={NavLink}
+                    color="white"
+                    size="md"
+                    py="2"
+                    width={{sm: "164px", md:"107px"}}
+                    fontSize={{sm:"h3Labels"}}
+                    lineHeight={{sm:"h3Labels"}}
+                    borderRadius="20"
+                    _activeLink={{
+                            bgColor:"$InstockBlack"
+
+                            }}>
+                    Inventory
+                </Link>
+                </Stack>
+            </Box>
+        </Flex>
     );
 }
 
